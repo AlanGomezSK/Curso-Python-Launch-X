f3cf5af36e3bf3ad75ea34a7f2bce71b649de897
AlanGomezSK <99113073+AlanGomezSK@users.noreply.github.com>
2022-02-11 00:37:50 -0600

    kata5
    add:kata5

---
+++ kata5/kata5.py
@@ -0,0 +1,9 @@
+#arithmetic operators in python
+#EXERCISE NUMBER ONE
+earth = 149597870
+jupiter = 778547200
+print(f"the difference of size between jupiter and earth is {(jupiter-earth)*.621} miles") 
+#EXERCISE NUMBER TWO
+first_planet=int(input("¿Say me the size of the fist planet?"))
+second_planet=int(input("¿Say me the sizze of the second planet?"))
+print(f"the difference of size between the planets are: {abs(first_planet-second_planet)}km, and in miles are :{abs((first_planet-second_planet)*.621)}m ")
